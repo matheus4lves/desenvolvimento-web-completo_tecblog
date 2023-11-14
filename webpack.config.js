6e5d12b7db83a46952339cdd36b67eb06587266a
Matheus Alves <matheusalves.bmd@gmail.com>
2023-11-13 22:40:58 -0300

    Update packages and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,44 @@
-const webpack = require('webpack')
-const path = require('path')
-const postcssPlugins = [
-  require('autoprefixer'),
-  require('postcss-import'),
-  require('postcss-mixins'),
-  require('postcss-nested'),
-  require('postcss-simple-vars')
-]
+const path = require("path");
+const { merge } = require("webpack-merge");
+// Use a relative path if the module/file is outside `node_modules`
+const parts = require("./webpack.parts");
 
-module.exports = {
-  mode: 'development',
-  entry: './app/assets/scripts/index.js',
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    before: function(app, server) {
-      server._watch('./app/**/*.html')
+/* "When multiple loaders are chained, it is important to remember that they are executed
+in reverse order – either right to left or bottom to top depending on array format."
+(Source: https://webpack.js.org/contribute/writing-a-loader/#complex-usage)
+
+"Loaders are evaluated/executed from right to left (or from bottom to top)."
+(Source: https://webpack.js.org/concepts/loaders/#configuration)
+*/
+const postcssPlugins = [
+  parts.postcssMixins(),
+  parts.postcssNested(),
+  parts.postcssSimpleVars(),
+  parts.autoprefixer(),
+  parts.postcssImport(),
+];
+
+const commonConfig = merge([
+  {
+    context: path.resolve(__dirname, "app"),
+    entry: "/assets/scripts/index.js",
+    output: {
+      path: path.resolve(__dirname, "docs"),
+      filename: "bundle.js",
     },
-    contentBase: path.join(__dirname, 'app'),
-    index: 'index.html',
-    host: '0.0.0.0',
-    port: 9003,
-    useLocalIp: true,
-    hot: true,
-    open: {
-      app: ['/opt/firefox-84.0b4/firefox/firefox', '--private-window']
-    }
   },
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        type: 'asset/source'
-      },
-      {
-        // inject images into the bundle
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', {loader: 'css-loader', options: {url: false}}, {loader: "postcss-loader", options: {postcssOptions: {plugins: postcssPlugins}}}]
-      },
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin({}),
-  ],
-  devtool: 'eval-source-map',
-  target: 'web'
-}
+  parts.loadHTML(),
+  parts.generateHTML({ template: "/index.html" }),
+  parts.loadImages(),
+  parts.loadCSS(postcssPlugins),
+]);
+
+const config = {
+  development: merge([{ devtool: "eval-source-map" }]),
+  production: merge([]),
+};
+
+module.exports = (_, argv) => {
+  const mode = argv.mode;
+  return merge([commonConfig, config[mode], { mode }]);
+};
---
+++ webpack.parts.js
@@ -0,0 +1,111 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+/* Keep in mind that loadHTML(), loadImages() and generateHTML() must be used together.
+Why? loadHTML() will convert loadable attributes (src, href, and so on) into
+require/import, which JavaScript can understand. Then, loadImages() will make
+webpack able to handle images, and generateHTML() will generate the final
+index.html file based on the template you provide. */
+exports.loadHTML = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+});
+
+exports.loadImages = () => ({
+  module: {
+    rules: [
+      {
+        // inject images into the bundle
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
+
+exports.generateHTML = ({ template } = {}) => ({
+  plugins: [new HtmlWebpackPlugin({ template })],
+});
+
+exports.loadCSS = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: postcssPlugins,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
+
+exports.loadCSS = (postcssPlugins = []) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", ...postcssPlugins],
+      },
+    ],
+  },
+});
+
+exports.postcssImport = () => ({
+  loader: "postcss-loader",
+  options: {
+    postcssOptions: {
+      plugins: [require("postcss-import")],
+    },
+  },
+});
+
+exports.autoprefixer = () => ({
+  loader: "postcss-loader",
+  options: {
+    postcssOptions: {
+      plugins: [require("autoprefixer")],
+    },
+  },
+});
+
+exports.postcssMixins = () => ({
+  loader: "postcss-loader",
+  options: {
+    postcssOptions: {
+      plugins: [require("postcss-mixins")],
+    },
+  },
+});
+
+exports.postcssNested = () => ({
+  loader: "postcss-loader",
+  options: {
+    postcssOptions: {
+      plugins: [require("postcss-nested")],
+    },
+  },
+});
+
+exports.postcssSimpleVars = () => ({
+  loader: "postcss-loader",
+  options: {
+    postcssOptions: {
+      plugins: [require("postcss-simple-vars")],
+    },
+  },
+});
